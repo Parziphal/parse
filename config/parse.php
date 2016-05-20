947002b630d0b8c6ad36054a77a68c14b5c02c09
Renzo <asterixvader@gmail.com>
2016-05-20 00:11:51 -0500

    Removed default values

--- config/parse.php
+++ config/parse.php
@@ -5,21 +5,21 @@ return [
     /**
      * Parse App Id
      */
-    'app_id' => env('PARSE_APP_ID', 'app_id'),
+    'app_id' => env('PARSE_APP_ID'),
 
     /**
      * Parse REST key
      */
-    'rest_key' => env('PARSE_REST_KEY', 'rest_key'),
+    'rest_key' => env('PARSE_REST_KEY'),
 
     /**
      * Parse Master Key
      */
-    'master_key' => env('PARSE_MASTER_KEY', 'master_key'),
+    'master_key' => env('PARSE_MASTER_KEY'),
 
     /**
      * Parse Server URL
      */
-    'server_url' => env('PARSE_SERVER_URL', 'server_url'),
+    'server_url' => env('PARSE_SERVER_URL'),
 
 ];
