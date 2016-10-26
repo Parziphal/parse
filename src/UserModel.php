9d9c096c34cd32dd78a1aea355c9cd09edd54ae9
Renzo <asterixvader@gmail.com>
2016-10-26 12:40:02 -0500

    Fixes
    - ParseUsers is used instead of ParseObject in constructor.
    - Overriden create static method to call signUp() instead of save().

--- src/UserModel.php
+++ src/UserModel.php
@@ -7,7 +7,7 @@ use Parse\ParseUser;
 class UserModel extends ObjectModel
 {
     protected static $parseClassName = '_User';
-    
+
     /**
      * These static methods in ParseUser return a new
      * instance of that class.
@@ -18,16 +18,58 @@ class UserModel extends ObjectModel
         'loginWithAnonymous',
         'become',
     ];
-    
+
     public static function __callStatic($method, array $params)
     {
         if (in_array($method, self::$parseUserStaticMethods)) {
-            return new static(call_user_func_array(ParseUser::class . '::' . $method, $params)); 
+            return new static(call_user_func_array(ParseUser::class . '::' . $method, $params));
         }
-        
+
         return parent::__callStatic($method, $params);
     }
-    
+
+    /**
+     * In order to create ParseUsers we have to call signUp instead of save.
+     */
+    public static function create($data, $useMasterKey = null)
+    {
+        if ($useMasterKey === null) {
+            $useMasterKey = static::$defaultUseMasterKey;
+        }
+
+        $model = new static($data, $useMasterKey);
+        $model->signUp();
+
+        return $model;
+    }
+
+    /**
+     * @param ParseUser|array  $data
+     * @param bool               $useMasterKey
+     */
+    public function __construct($data = null, $useMasterKey = null)
+    {
+        if ($data != null && !$data instanceof ParseUser && !is_array($data)) {
+            $type = is_object($data) ? get_class($data) : gettype($data);
+
+            throw new Exception(
+                sprintf("Either a ParseUser or an array must be passed to instantiate a UserModel, %s passed", $type)
+            );
+        }
+
+        if ($data instanceof ParseUser) {
+            $this->parseObject = $data;
+        } else {
+            $this->parseObject = new ParseUser(static::parseClassName());
+
+            if ($data) {
+                $this->fill($data);
+            }
+        }
+
+        $this->useMasterKey = $useMasterKey !== null ? $useMasterKey : static::$defaultUseMasterKey;
+    }
+
     public function linkWithFacebook($id, $accessToken, $expirationDate = null, $useMasterKey = false)
     {
         return new static($this->parseObject->loginWithAnonymous(
