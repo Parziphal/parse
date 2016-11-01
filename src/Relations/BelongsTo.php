234b64e8d6f7a3c56fe3d49299275f63b57b3dae
Renzo <asterixvader@gmail.com>
2016-11-01 10:14:33 -0500

    Fetch object upon relation load

--- src/Relations/BelongsTo.php
+++ src/Relations/BelongsTo.php
@@ -7,22 +7,22 @@ use Parziphal\Parse\ObjectModel;
 class BelongsTo extends Relation
 {
     protected $embeddedClass;
-    
+
     protected $keyName;
-    
+
     protected $childObject;
-    
+
     public function __construct($embeddedClass, $keyName, ObjectModel $childObject)
     {
         $this->embeddedClass = $embeddedClass;
         $this->childObject   = $childObject;
         $this->keyName       = $keyName;
     }
-    
+
     public function getResults()
     {
         $class = $this->embeddedClass;
-        
-        return new $class($this->childObject->getParseObject()->get($this->keyName));
+
+        return (new $class($this->childObject->getParseObject()->get($this->keyName)))->fetch();
     }
 }
