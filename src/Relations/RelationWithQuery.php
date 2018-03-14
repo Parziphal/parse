dcffc4583c1f0065d4b06967f81dfdad64e4679b
Renzo <asterixvader@gmail.com>
2018-03-14 00:32:43 -0500

    No spaces

--- src/Relations/RelationWithQuery.php
+++ src/Relations/RelationWithQuery.php
@@ -8,22 +8,22 @@ use Parziphal\Parse\ObjectModel;
 abstract class RelationWithQuery extends Relation
 {
     protected $query;
-    
+
     /**
      * @param ObjectModel
      */
     protected $parentObject;
-    
+
     abstract protected function addConstraints();
-    
+
     public function __construct(Query $query, ObjectModel $parentObject)
     {
-        $this->query        = $query;
+        $this->query = $query;
         $this->parentObject = $parentObject;
 
         $this->addConstraints();
     }
-    
+
     public function __call($method, $parameters)
     {
         $result = call_user_func_array([$this->query, $method], $parameters);
